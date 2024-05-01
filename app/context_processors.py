bd3d5dfd9346845732fa1fc03a45293bf123bf4b
Georgiy <37848058+SivikGosh@users.noreply.github.com>
2024-05-01 16:29:33 +0300

    fixed static

--- app/context_processors.py
+++ app/context_processors.py
@@ -1,5 +1,17 @@
 from datetime import datetime
+from app.models import Report
 
 
 def current_month(request):
     return {'current_month': datetime.now().month}
+
+
+def newest_date_reports(request):
+    dates = [
+        str(date[0]) for date
+        in Report.objects.values_list('date').order_by('-date').distinct()
+    ]
+    try:
+        return {'newest_date': dates[0]}
+    except IndexError:
+        return {'newest_date': datetime.now().date}
